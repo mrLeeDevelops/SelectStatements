965f1f916dfbe0aecf18f8ac7252090e9fe0558f
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-13 12:28:08 -0500

    Initial Commit

---
+++ SelectStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelectStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 599;
+            Console.WriteLine("Can you guess what my favorite number is between 1 and 1000?");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("CORRECT! Today must be your lucky day!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("TOO LOW");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine("TOO HIGH");
+            }
+            else Console.WriteLine("Nevermind! Stop Guessing.");
+        }
+    }
+}
